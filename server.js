a8a3f790d61bd0136f72a3b12c3d5e0cf1f90056
webFarrock <webFarrock@mail.ru>
2016-12-27 16:55:18 +0300

    Lecture45 finished

--- server.js
+++ server.js
@@ -1,19 +1,59 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
 
 let app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    
+    let now = new Date().toString();
+    
+    let log = `${now}: ${req.method} ${req.url}`;
+    
+    console.log(log);
+    fs.appendFile('server.log', log + "\n", (err) = {
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    
+    next();
+});
+
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello express</h1>');
 
-    res.send({
-        name: 'Pavel',
-        likes: ['one', 'two', 'three'],
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome message',
     });
 
 });
 
 app.get('/about', (req, res) => {
-    res.send('about this app');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
 });
 
 app.get('/bad', (req, res) => {
@@ -22,5 +62,7 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
 
